d765623d44946f7b7f1249cde84f93b1c9bb8d11
MartinaGeorgieva17 <martina.1990.1990@gmail.com>
2024-10-02 18:12:59 +0300

    adding_info

--- 42. Excersise/app.py
+++ 42. Excersise/app.py
@@ -4,19 +4,16 @@ from Ui_LoginForm import Ui_Form
 from Ui_Buttons import UiButtons
 
 
-
-
 class MainWindow(qtw.QWidget, Ui_Form):
     def __init__(self, *args, **kwargs):
-        super(). __init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.setupUi(self)
-
+        self.label.setText('Changed @@@@@@@@@@@@')
 
 if __name__ == "__main__":
-    app = qtw.QApplication(sys.argv) 
+    app = qtw.QApplication(sys.argv)
 
+    mainWindow = MainWindow()
+    mainWindow.show()
 
-mainWindow = MainWindow()
-mainWindow.show()
-
-sys.exit(app.exec())
+    sys.exit(app.exec())
